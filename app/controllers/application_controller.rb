2d548c02710d19fc4a1780c15d71d3c42b982aa1
yoshitaro2019 <yomi_majoko@yahoo.co.jp>
2019-11-15 12:29:46 +0900

    add_devise&flash_method&change_japanese
    deviseの機能追加
    (1)models/user.rb
    (2)controllers/users_contoller.rb
    (3)users/edit.html.haml
    (4)users/devise/registrations
         users/devise/sessions
         users/devise/shared
    
    flashメソッドの追加
    (1)view/layouts/_flash.html.haml
         view/layouts/application.html.haml
    (2)stylesheets/config/_color.scss
         stylesheets/modules/_flash.scss
         stylesheets/application.scss
    
    flashメソッドの日本語化
    (1)config/locales/detavase.ja.yml
         config/locales/ja.yml
         config/application.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # ログインしていなかったら自動でログイン画面へ(deviseのメソッド)
+  before_action :authenticate_user!
+  # もしdeviseに関わる何かが動いたら下記変数が動く
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters  
+    # deviseのメソッド(sign_up時のkey[:name]の許可)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
