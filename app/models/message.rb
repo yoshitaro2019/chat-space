162128e2e466eeb304312b4280bf1949404d5fdf
yoshitaro2019 <yomi_majoko@yahoo.co.jp>
2019-11-21 13:06:59 +0900

    add-message_send_function(2)
    (1)messages_controller.rb
    ・index/createアクションの設定
    ・メッセージ送信時、失敗時のflash設定
    ・createアクションのストロングパラメータ設定
    （group/messageのネストでmargeあり）
    ・set_groupアクションの設定
    （該当グループfindメソッド）
    
    (2)group/index.html.hamlのrenderメソッド変更（side_barのみの表示に変更）
    
    (3)_main_chat.html.hamlのform_forタグの修正
    ※念のため、元のファイル残しておくので最終的には削除する

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
-
+  # もしimageが空じゃなければ、contentは空にできない(もしimageがあれば、contentは空でもいい)
   validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
 end
